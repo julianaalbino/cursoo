46f30876052e4c5411f316f6f0887cd5694809a1
Pedro Henrique <peucopasa@gmail.com>
2019-10-23 01:02:10 -0300

    Pagina htlm

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Inicial</title>
+	<link rel="stylesheet" type="text/css" href="public/css/index.css">
+</head>
+<body>
+	<center>
+		<br><br><br><br><a class="h3" href="#"><span>Pular </span></a><br><br>
+		<br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+		<img src="public/icones/LOGO.png" width="80%" height="80%">
+		<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
+		<div class="form">
+			<br><br><br><br><br><br>
+			<a class="button1" href="view/telaLogin.php">Entrar</a><br><br>
+			<a class="button2" href="view/telaCadastro.php">Inscrever-se</a>
+			<br><br><br><br><br>
+			<a>Entrar com</a> &emsp; &emsp; 
+			<a href="#"><img src="public/icones/google.png" width="10%" height="12%"></a>&emsp;&emsp;
+			<a href="#"><img src="public/icones/face.png" width="10%" height="12%"></a>
+		</div>
+	</center>
+</body>
+</html>
